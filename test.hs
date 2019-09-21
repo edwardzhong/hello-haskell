ae08991329278e0f88f08742fb6ceb57f391c691
edwardzhong <edwardloveyou@gmail.com>
2019-09-21 12:35:00 +0800

    format

--- test.hs
+++ test.hs
@@ -1,23 +1,26 @@
 {-# LANGUAGE TemplateHaskell #-}
-import           Control.Applicative -- Const
-import           Control.Monad -- MonadPlus , guard
+
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.Identity
+import           Control.Monad.State
 import           Control.Monad.Trans.Maybe
 import           Control.Monad.Writer
-import           Control.Monad.State
-import           Control.Monad.Identity
 import           Data.Char
 import           Language.Haskell.TH
-import           Debug.Trace
 
 -- import           Text.Parsec
-data Book = Book { 
-        bid::Int, 
-        bname::String 
-} deriving(Show,Read,Eq)
+data Book =
+  Book
+    { bid   :: Int
+    , bname :: String
+    }
+  deriving (Show, Read, Eq)
 
-isPassValid::String -> Bool
+isPassValid :: String -> Bool
 isPassValid s = length s >= 8 && check s
-        where check s = and [f s | f <- any <$> [isUpper,isLower,isNumber]]
+  where
+    check s = and [f s | f <- any <$> [isUpper, isLower, isNumber]]
 
 -- setPassword::MaybeT (WriterT String IO)()
 -- setPassword = do
@@ -27,57 +30,54 @@ isPassValid s = length s >= 8 && check s
 --         guard $ isPassValid pass
 --         tell pass
 -- runWriterT $ runMaybeT setPassword
-
-setPassword::MaybeT IO String
+setPassword :: MaybeT IO String
 setPassword = do
-        liftIO $ putStrLn "Please set a password"
-        pass <- liftIO $ getLine
-        guard $ isPassValid pass
-        return pass
---runMaybeT setPassword
+  liftIO $ putStrLn "Please set a password"
+  pass <- liftIO $ getLine
+  guard $ isPassValid pass
+  return pass
 
-push::Int -> MaybeT (State [Int]) Int
+--runMaybeT setPassword
+push :: Int -> MaybeT (State [Int]) Int
 -- push x = MaybeT $ state $ \xs -> (Just x,x:xs)
 push x = do
-        xs <- lift get
-        lift $ put $ x:xs
-        return x
--- (runState $ runMaybeT $ push 1) [1,2,3]
+  xs <- lift get
+  lift $ put $ x : xs
+  return x
 
-pop::MaybeT (State [Int]) Int
+-- (runState $ runMaybeT $ push 1) [1,2,3]
+pop :: MaybeT (State [Int]) Int
 -- pop = MaybeT $ state $ \xs -> case xs of
 --                                 [] ->  (Nothing,xs)
 --                                 (y:ys) -> (Just y,ys)
 pop = do
-        xs <- lift get
-        case xs of
-                []     -> do 
-                        lift $ put []
-                        fail "err"
-                (y:ys) -> lift $ put ys >> return y
+  xs <- lift get
+  case xs of
+    [] -> do
+      lift $ put []
+      fail "err"
+    (y:ys) -> lift $ put ys >> return y
+
 -- (runState $ runMaybeT $ pop ) [1,2,3]
-
-stack::MaybeT (State [Int]) Int
+stack :: MaybeT (State [Int]) Int
 stack = do
-        push 1
-        -- push 2
-        a <- pop
-        b <- pop
-        return b
-        
--- (runState $ runMaybeT stack) []
+  push 1
+--   push 2
+  a <- pop
+  b <- pop
+  return b
 
+-- (runState $ runMaybeT stack) []
 guardTest :: Int -> IO ()
 guardTest a = guard (a > 5) >> putStrLn "larger than 5"
 
 whenTest :: Int -> IO ()
 whenTest a = when (a > 5) $ putStrLn "larger than 5"
 
--- mklen:: DecsQ
-mklen::Q()
-mklen = do
-        TyConI (DataD _ _ _ _ cons _) <- reify ''Book
-        [RecC conName fields] <- return cons
-        let names = forM fields (\(fileName,_,fileType) -> print $ show $ (nameBase fileName) >> return fileName)
-        return ()
+-- ref :: DecsQ
+ref :: Q [String]
+ref = do
+  TyConI (DataD _ _ _ _ cons _) <- reify ''Book
+  [RecC conName fields] <- return cons
+  return $ (\(fileName, _, fileType) -> nameBase fileName) <$> fields
 -- TyConI (DataD [] Ghci1.Book [] Nothing [RecC Ghci1.Book [(Ghci1.bid,Bang NoSourceUnpackedness NoSourceStrictness,ConT GHC.Types.Int),(Ghci1.bname,Bang NoSourceUnpackedness NoSourceStrictness,ConT GHC.Base.String)]] [])
